56b640d496fc11c5f6981676b209c0d133738b56
PabloJRW <ramoswilkinspj28gmail.com>
2024-10-16 13:37:49 -0500

    script ready.

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,45 +1,81 @@
+import os
 import requests
-import datetime as dt
+import pandas as pd
 
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
 }
 
-url_api = "https://drop-api.ea.com/rating/ea-sports-fc?locale=en&limit=100"
-response = requests.get(url_api,headers=headers)
 
-data = response.json()
+num_of_pages = 176   # The web page has 175 pages of players
+offset = 100   # Each page has 100 players
+players_list = []
+for i in range(num_of_pages):
+    if i == 0:
+        url_api = "https://drop-api.ea.com/rating/ea-sports-fc?locale=en&limit=100"
+    else:
+        url_api = f"https://drop-api.ea.com/rating/ea-sports-fc?locale=en&limit=100&offset={offset}"
 
-players = data["items"][:2]
+    try:
+        response = requests.get(url_api,headers=headers)
+        response.raise_for_status()
 
-for player in players:
-    print(player["firstName"])
-    print(player["lastName"])
-    print(player["commonName"])
-    print(player["gender"]["id"])
-    print(player["height"])
-    print(player["weight"])
-    print(player["nationality"]["label"])
-    print(player["birthdate"])
-    print(player["position"]["positionType"]["id"])
-    print(player["position"]["shortLabel"])
-    alternatePositions = [position["shortLabel"] for position in player["alternatePositions"]]
-    print(alternatePositions)
-    print(player["preferredFoot"])
-    print(player["leagueName"])
-    print(player["team"]["label"])
-    for ability_info in player["playerAbilities"]:
-        ability_name = ability_info["label"]
-        print(ability_name)
+        data = response.json()
+        players = data["items"]
 
-    for stat_name, stat_info in player["stats"].items():
-        value = stat_info["value"]
-        print(f"{stat_name}: {value}")
+        for player in players:
+            try:
+                player_dict = {
+                    "first_name": player["firstName"],
+                    "last_name": player["lastName"],
+                    "common_name": player["commonName"],
+                    "avatar_url": player["avatarUrl"],
+                    "shield_url": player["shieldUrl"],
+                    "gender_id": player["gender"]["id"],
+                    "height": player["height"],
+                    "weight": player["weight"],
+                    "nationality": player["nationality"]["label"],
+                    "birthdate": player["birthdate"],
+                    "position_type": player["position"]["positionType"]["name"],
+                    "position_short_label": player["position"]["shortLabel"],
+                    "position_label": player["position"]["label"],
+                    "alternate_positions": ", ".join([position["shortLabel"] for position in player["alternatePositions"]]),  # Convertir lista a cadena
+                    "preferred_foot": player["preferredFoot"],
+                    "league_name": player["leagueName"],
+                    "team_label": player["team"]["label"],
+                    "team_image_url": player["team"]["imageUrl"],
+                    "player_abilities": ", ".join([ability_info["label"] for ability_info in player["playerAbilities"]]),  # Convertir lista a cadena
+                    "weak_foot_ability": player["weakFootAbility"],
+                    "skill_moves": player["skillMoves"],
+                    "overall_rating": player["overallRating"]
+                }
+            
+                # Flatten the stats dictionary to include it in player_dict
+                for stat_name, stat_info in player["stats"].items():
+                    player_dict[f"stat_{stat_name}"] = stat_info["value"]
+
+                players_list.append(player_dict)
+
+            except KeyError as e:
+                print(f"Key error: {e} - skipping player.")
+            except Exception as e:
+                print(f"Unexpected error: {e} - Skipping player.")
     
-    print(player["weakFootAbility"])
-    print(player["skillMoves"])
+    except requests.RequestException as e:
+        print(f"Request failed at iteration {i}: {e}")
+    except Exception as e:
+        print(f"Unexpected error at iteration {i}: {e}")
+
     
-    print(player["overallRating"])
-    
-    print()
+    offset += 100 # Increment the offset for the next page
+
+
+# Convert the list of dictionaries into a Pandas DataFrame
+df = pd.DataFrame(players_list)
+
+# Save the players data into a CSV file after each iteration
+file_name = 'fcplayers_0'
+df.to_csv(os.path.join('data','raw', f"{file_name}.csv"), encoding='utf-8', index=False)
+
+print(f"Data saved to {file_name}.csv")
---
+++ tests/test.py
@@ -0,0 +1,53 @@
+import requests
+import datetime as dt
+
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+}
+
+url_api = "https://drop-api.ea.com/rating/ea-sports-fc?locale=en&limit=100"
+response = requests.get(url_api,headers=headers)
+
+data = response.json()
+
+players = data["items"][:2]
+
+for player in players:
+    print(player["firstName"])
+    print(player["lastName"])
+    print(player["commonName"])
+    print(player["avatarUrl"])
+    print(player["shieldUrl"])
+    print(player["gender"]["id"])
+    print(player["height"])
+    print(player["weight"])
+    print(player["nationality"]["label"])
+    print(player["birthdate"])
+    print(player["position"]["positionType"]["name"])
+    print(player["position"]["shortLabel"])
+    print(player["position"]["label"])
+    alternatePositions = [position["shortLabel"] for position in player["alternatePositions"]]
+    print(alternatePositions)
+    print(player["preferredFoot"])
+    print(player["leagueName"])
+    #print(player)
+    print(player["team"]["label"])
+    print(player["team"]["imageUrl"])
+    for ability_info in player["playerAbilities"]:
+        ability_name = ability_info["label"]
+        print(ability_name)
+
+    for stat_name, stat_info in player["stats"].items():
+        value = stat_info["value"]
+        print(f"{stat_name}: {value}")
+    
+    print(player["weakFootAbility"])
+    print(player["skillMoves"])
+    
+    print(player["overallRating"])
+    
+    print()
+    print()
+    print()
+    
