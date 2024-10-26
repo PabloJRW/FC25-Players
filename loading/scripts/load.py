0a1f4e091475f35484f7b10a0069257f171c418b
PabloJRW <ramoswilkinspj28gmail.com>
2024-10-26 11:11:36 -0500

    some credentials modifications

--- loading/scripts/load.py
+++ loading/scripts/load.py
@@ -1,8 +1,10 @@
 import os
 import pandas as pd
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
-# 
+
+# Paths to dataframes
 players = os.path.join('transformation','transformed_data','player_data.csv')
 player_abilities = os.path.join('transformation','transformed_data','player_abilities_data.csv')
 nationality = os.path.join('transformation','transformed_data','nationality_data.csv')
@@ -10,6 +12,8 @@ player_technical = os.path.join('transformation','transformed_data','player_tech
 stats = os.path.join('transformation','transformed_data','stats_data.csv')
 team = os.path.join('transformation','transformed_data','team_data.csv') 
 
+
+# Loading all the dataframes
 players = pd.read_csv(players)
 player_abilities = pd.read_csv(player_abilities)
 nationality = pd.read_csv(nationality)
@@ -17,17 +21,21 @@ player_technical = pd.read_csv(player_technical)
 stats = pd.read_csv(stats)
 team = pd.read_csv(team)
 
-# Configurar la cadena de conexión para PostgreSQL
-usuario = 'postgres'
-contraseña = 'base'
-host = 'localhost'  # Cambia esto si el servidor no está en local
-puerto = '5432'     # Puerto predeterminado para PostgreSQL
-base_datos = 'fc25_players'
 
-# Crear la conexión a la base de datos PostgreSQL
-engine = create_engine(f'postgresql://{usuario}:{contraseña}@{host}:{puerto}/{base_datos}')
+# Load the environment variables from .env file
+load_dotenv()
 
-# Guardar el DataFrame en la base de datos
+# Invoke credentials
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+
+# Create connection to PostgreSQL database
+engine = create_engine(f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+
+# Save the Dataframes into the database
 players.to_sql('players', con=engine, if_exists='replace', index=False)
 player_abilities.to_sql('abilities', con=engine, if_exists='replace', index=False)
 stats.to_sql('stats', con=engine, if_exists='replace', index=False)
@@ -35,5 +43,5 @@ team.to_sql('teams', con=engine, if_exists='replace', index=False)
 nationality.to_sql('nationality', con=engine, if_exists='replace', index=False)
 player_technical.to_sql('technical', con=engine, if_exists='replace', index=False)
 
-# Cerrar la conexión
+# Close connection
 engine.dispose()
